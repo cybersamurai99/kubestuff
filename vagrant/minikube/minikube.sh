a441910edc84b1cf6b3dee57d4214545538250fa
Nico Kruger <linico.nicokruger@gmail.com>
2022-11-18 19:38:31 +0100

    fixed minikube deb config

--- vagrant/minikube/minikube.sh
+++ vagrant/minikube/minikube.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-sudo apt-get -y install ca-certificates curl gnupg lsb-release
-sudo mkdir -p /etc/apt/keyrings
+apt-get -y install ca-certificates curl gnupg lsb-release
+mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-thisArch=$(dpkg --print-architecture)
-echo "deb [arch=$thisArch signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu stable" > /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo groupadd docker
-sudo usermod -aG docker nico
+chmod a+r /etc/apt/keyrings/docker.gpg
+#thisArch=$(dpkg --print-architecture)
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+groupadd docker
+usermod -aG docker nico
 newgrp docker
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-sudo systemctl enable docker.service
+systemctl enable docker.service
+systemctl enable containerd.service
+systemctl enable docker.service
 
 ## install minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+install minikube-linux-amd64 /usr/local/bin/minikube
 #minikube start
 
 ## add autocomplete and kubectl alias in ~/.bashrc
+cat << EOF > /home/nico/.bashrc
 alias kubectl="minikube kubectl --"
 source <(kubectl completion bash)
 source <(minikube completion bash)
-
+EOF
 
 ## create minikube service /etc/systemd/system/minikube.service
 
--- vagrant/minikube/script.sh
+++ vagrant/minikube/script.sh
@@ -3,17 +3,17 @@
 #apt-get update
 #apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 # add nico user
-sudo adduser nico --gecos "Nico Kruger,RoomNumber,WorkPhone,HomePhone" --disabled-password
+adduser nico --gecos "Nico Kruger,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "nico:nico@123" | sudo chpasswd
 # add nico pub key
 mkdir -p /home/nico/.ssh
 echo "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEED62Zh6X7y60Wjbs0ZUaC0ydIh2Q7VUI1SBzQhz1YcgB2NTxkYkhhfVdgG6bcBE4sft7PlGim2hvYtlRj95zY= linic@cyberx" >> /home/nico/.ssh/authorized_keys
 chmod -R go= /home/nico/.ssh
 chown -R nico:nico /home/nico/.ssh
-echo 'nico ALL=(ALL) NOPASSWD:ALL' > /target/etc/sudoers.d/admins
+echo 'nico ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/admins
 
 # install mdns
 #hostnamectl set-hostname ubuntu
-sudo apt install avahi-daemon -y
+apt install avahi-daemon -y
 systemctl start avahi-daemon
 systemctl enable avahi-daemon
